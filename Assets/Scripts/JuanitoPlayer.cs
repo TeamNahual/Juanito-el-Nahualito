445a56f802375651d981ffebdef76ab5f408463b
CarpeSwag <james@garbagnati.com>
2017-11-28 14:14:59 -0800

    Starting NavMesh tutorial.

---
+++ Assets/Scripts/JuanitoPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class JuanitoPlayer : MonoBehaviour {
+	public Transform target;
+	NavMeshAgent agent;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		agent.SetDestination(target);
+	}
+}
