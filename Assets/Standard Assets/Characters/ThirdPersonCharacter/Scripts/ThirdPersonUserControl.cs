5b92cb8915f8f9a8ffddb17fa4dd2e18ca33c402
CarpeSwag <james@garbagnati.com>
2018-01-20 21:01:09 -0800

    Fixed controller input.

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -48,7 +48,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+			bool keyboard = !(Mathf.Abs(h) < 0.1f && Mathf.Abs(v) < 0.1f);
+			bool crouch = Input.GetKey(KeyCode.C);
+			
+			if (!keyboard)
+			{
+				h = CrossPlatformInputManager.GetAxis("Horizontal-Joystick");
+				v = CrossPlatformInputManager.GetAxis("Vertical-Joystick");
+			}
 
             // calculate move direction to pass to character
             if (m_Cam != null)
