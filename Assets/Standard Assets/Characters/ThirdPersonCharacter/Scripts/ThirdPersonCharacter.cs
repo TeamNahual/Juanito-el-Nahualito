48eb8496559cbe9ead5be3688ca6ec72f93bb732
CarpeSwag <james@garbagnati.com>
2018-01-20 20:30:21 -0800

    Adjusted camera zoom settings.

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -42,8 +42,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
 			
-			
-			
 			// Set game camera to player
 			if (Camera.main != null)
             {	
