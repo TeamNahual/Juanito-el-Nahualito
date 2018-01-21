04b508f86e1115d03dbf001918d82cead34928c6
Christopher Huynh <chphuynh@ucsc.edu>
2018-01-20 22:19:06 -0800

    Pushed Spirit Transformation and AIFollow

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	void Start()
+	{
+
+	}
+
+	void OnCollisionStay(Collision other)
+	{
+		if(other.collider.tag == "Player")
+		{
+			other.gameObject.transform.parent = transform.parent;
+		}
+	}
+
+	void OnCollisionExit(Collision other)
+	{
+		if(other.collider.tag == "Player")
+		{
+			other.gameObject.transform.parent = null;
+		}
+	}
+}
---
+++ Assets/Scripts/SpiritManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine;
+
+public class SpiritManager : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Spirit;
+
+	private float spirit_start_time;
+	private float spirit_time_limit = 30;
+	private bool SpiritState = false;
+
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreCollision(Player.GetComponent<Collider>(), Spirit.GetComponent<Collider>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpiritHandler();
+	}
+
+ 	private void EnterSpiritState()
+ 	{
+ 		Player.GetComponent<ThirdPersonUserControl>().enabled = false;
+ 		Player.GetComponent<ThirdPersonCharacter>().enabled = false;
+
+ 		Spirit.transform.position = Player.transform.position;
+ 		Spirit.SetActive(true);
+ 		SpiritState = true;
+
+ 	}
+
+ 	private void SpiritHandler()
+ 	{
+ 		if(Input.GetKeyDown(KeyCode.A))
+		{
+			if(!SpiritState)
+			{
+				EnterSpiritState();
+				spirit_start_time = Time.time;
+			}
+			else
+			{
+				EndSpiritState();
+			}
+		}
+
+		if(SpiritState && Time.time - spirit_start_time > spirit_time_limit)
+		{
+			EndSpiritState();
+		}
+ 	}
+
+ 	private void EndSpiritState()
+ 	{
+ 		Spirit.SetActive(false);
+ 		Player.GetComponent<ThirdPersonUserControl>().enabled = true;
+ 		Player.GetComponent<ThirdPersonCharacter>().enabled = true;
+ 		SpiritState = false;
+ 	}
+}
