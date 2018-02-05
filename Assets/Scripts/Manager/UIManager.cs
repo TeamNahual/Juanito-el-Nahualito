e234d51c2c174a81f244b44fa9e17065c5629058
CarpeSwag <james@garbagnati.com>
2018-02-04 22:54:34 -0800

    Created a simple UI Manager.

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
-	public static GameManager instance = null; // Allows us to access this from other scripts
+	public static UIManager instance = null; // Allows us to access this from other scripts
     private bool isMenuOpen;
 	private bool menuToggleProtect;
 	public GameObject inGameMenu; 
@@ -17,15 +17,13 @@ public class GameManager : MonoBehaviour
 			instance = this;
 		else if (instance != this)
 			Destroy(gameObject);
-		// Uncomment to keep game manager persistent
-		//DontDestroyOnLoad(gameObject);
 		isMenuOpen = false;
 		menuToggleProtect = false;
 	}
 	
     void Start()
     {
-		DynamicGI.UpdateEnvironment();
+		DynamicGI.UpdateEnvironment(); // Get rid of this eventually
 	}
 	
 	void Update()
@@ -49,6 +47,6 @@ public class GameManager : MonoBehaviour
 	
 	public void reloadScene()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);isMenuOpen = false;
+		isMenuOpen = false;
 	}
 }
