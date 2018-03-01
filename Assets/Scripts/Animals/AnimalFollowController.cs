f7fd837643b532cbd79d7f2924352d9707946242
Christopher Huynh <chphuynh@ucsc.edu>
2018-02-28 22:47:26 -0800

    Testing Waypoint stuff

---
+++ Assets/Scripts/Animals/AnimalFollowController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+
+using MalbersAnimations.Events;
+using MalbersAnimations.Utilities;
+using MalbersAnimations;
+
+public class AnimalFollowController : MonoBehaviour {
+
+	public AnimalType animal;
+	public float CollisionRadius;
+	public GameObject[] WayPoints;
+
+	[HideInInspector]
+	public AnimalAIControl aiController;
+
+	protected bool following;
+	public bool traveling;
+	protected int wayPointCount = 0;
+	protected GameObject currentWaypoint;
+
+	// Use this for initialization
+	void Start () {
+		//aiController = GetComponent<AICharacterControl>();
+		aiController = GetComponent<AnimalAIControl>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!traveling)
+			CheckForPlayer ();
+		else
+		{
+			if (currentWaypoint != null)
+			{
+				if (Vector3.Distance (transform.position, currentWaypoint.transform.position) < aiController.stoppingDistance + 1)
+				{
+					Debug.Log ("Reached " + currentWaypoint.name);
+					traveling = false;
+					currentWaypoint = null;
+				}
+			}
+		}
+	}
+
+	protected void CheckForPlayer()
+	{
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, CollisionRadius);
+
+		if (hitColliders.Length != 0)
+		{
+			foreach (Collider hit in hitColliders)
+			{
+				if (!Juanito.ins.SpiritState)
+				{
+					// Flee to Waypoint if Juanito is Human
+					if (hit.gameObject == Juanito.ins.JuanitoHuman)
+					{
+						currentWaypoint = WayPoints [wayPointCount];
+						aiController.SetTarget (currentWaypoint.transform);
+						wayPointCount = (wayPointCount + 1 > WayPoints.Length - 1? 0 : wayPointCount + 1);
+						traveling = true;
+						return;
+					}
+
+				} else
+				{
+
+					// Follow Juanito if Juanito is Spirit
+					if (hit.gameObject == Juanito.ins.JuanitoSpirit)
+					{
+						aiController.SetTarget (hit.gameObject.transform);
+						following = true;
+						Juanito.ins.SpiritControl.currentFollower = transform.gameObject;
+						return;
+					}
+				}
+			}
+		}
+		else
+		{
+			Debug.Log("No collisions!");
+		}
+
+		//following = false;
+		//aiController.SetTarget(null);
+
+	} 
+
+}
