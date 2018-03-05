2f72b170282709c38a59ef6ea4454c9cb2a234e4
Christopher Huynh <chphuynh@ucsc.edu>
2018-03-04 22:15:46 -0800

    Left/Right Pushing

---
+++ Assets/Scripts/Butterfly Puzzle/ButterflyBody.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyBody : MonoBehaviour {
+
+	public ButterflyPiece main;
+	public int directionFlag;
+
+	// Use this for initialization
+	void Start () {
+		main = transform.parent.gameObject.GetComponent<ButterflyPiece>();
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject == Juanito.ins.JuanitoHuman)
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				if(main.isPushing)
+				{
+					main.DetachPlayer();
+					main.isPushing = false;
+				}
+				else
+				{
+					main.AttachPlayer();
+					main.isPushing = true;
+					main.directionFlag = directionFlag;
+					main.movementVector = transform.right;
+				}
+			}
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Butterfly Puzzle/ButterflyPiece.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ButterflyPiece : MonoBehaviour {
+
+	public bool isPushing = false;
+	public int directionFlag;
+	public Vector3 movementVector;
+
+	public float pushSpeed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isPushing)
+			PushHandler();
+	}
+
+	void PushHandler()
+	{
+		float[] playerInput = GetPlayerInput();
+
+		transform.Translate(playerInput[1] * movementVector * directionFlag * pushSpeed);
+	}
+
+	float[] GetPlayerInput()
+	{
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+		bool keyboard = !(Mathf.Abs(h) < 0.1f && Mathf.Abs(v) < 0.1f);
+
+		if (!keyboard)
+		{
+			h = CrossPlatformInputManager.GetAxis("Horizontal-Joystick");
+			v = CrossPlatformInputManager.GetAxis("Vertical-Joystick");
+		}
+
+		return new float[] {h,v};
+	}
+
+	public void AttachPlayer()
+	{
+		Juanito.ins.transform.parent = transform;
+		Juanito.ins.JuanitoHuman.GetComponent<ThirdPersonUserControl>().enabled = false;
+ 		Juanito.ins.JuanitoHuman.GetComponent<ThirdPersonCharacter>().enabled = false;
+	}
+
+	public void DetachPlayer()
+	{
+		Juanito.ins.transform.parent = null;
+		Juanito.ins.JuanitoHuman.GetComponent<ThirdPersonUserControl>().enabled = true;
+ 		Juanito.ins.JuanitoHuman.GetComponent<ThirdPersonCharacter>().enabled = true;
+	}
+}
