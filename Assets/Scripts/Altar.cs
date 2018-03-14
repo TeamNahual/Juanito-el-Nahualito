8267ca5adaeefa86c97609dd22cddbd56b0c8f4a
Christopher Huynh <chphuynh@ucsc.edu>
2018-03-14 16:12:58 -0700

    Some altar stuff

---
+++ Assets/Scripts/Altar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour {
+
+	public bool SeashellRelic;
+	public bool TorusRelic;
+	public bool NecklaceRelic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/AltarPiece.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarPiece : MonoBehaviour {
+
+	public bool SeashellRelic;
+	public bool TorusRelic;
+	public bool NecklaceRelic;
+
+	public Altar main;
+
+	// Use this for initialization
+	void Start () {
+		main = transform.parent.gameObject.GetComponent<Altar>();
+
+		if(NecklaceRelic)
+		{
+			GetComponent<Collider>().enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
