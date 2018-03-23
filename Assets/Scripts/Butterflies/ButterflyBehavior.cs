c0122391421208e6a314da984d9e8c4d7a3fb5f6
CarpeSwag <james@garbagnati.com>
2018-03-23 10:35:47 -0700

    Adjusted butterfly flight paths to be more orbital instead of free form.

---
+++ Assets/Scripts/Butterflies/ButterflyBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflyBehavior : MonoBehaviour
+{
+    // Reference to the controller.
+    public ButterflySpawner controller;
+
+	// Origin of orbit
+	public Vector3 origin;
+	
+	// Velocity
+	public float velBase = 10.0f;
+	public float velVariation = 10.0f;
+	
+    // Randoms
+	Vector3 randVector;
+    float randVel;
+    float randOffset;
+
+    void Start()
+    {
+        randOffset = Random.value * 50.0f;
+		
+		randVel = Random.Range(velBase - velVariation, velBase + velVariation);
+		Debug.Log(randVel);
+		
+		Renderer rend = GetComponent<Renderer>();
+        rend.material.SetFloat("_AnimOffset", Random.value * 10.0f);
+		
+		randVector = new Vector3(
+			(Random.value > 0.5)? Random.Range(2.5f, 5.0f): Random.Range(-5.0f, -2.5f),
+			(Random.value > 0.5)? Random.Range(2.5f, 5.0f): Random.Range(-5.0f, -2.5f),
+			(Random.value > 0.5)? Random.Range(2.5f, 5.0f): Random.Range(-5.0f, -2.5f)
+		);
+    }
+
+    void Update()
+    {
+		// Calculate next position
+		var time = Time.time / randVel + randOffset;
+		var nextPosition = new Vector3(
+			Mathf.Sin( (origin.x + randVector.x) * time ),
+			Mathf.Cos( (origin.y + randVector.y) * time ) + 1,
+			Mathf.Cos( (origin.z + randVector.z) * time )
+		);
+
+        // Move and Rotate butterflies
+		transform.rotation = Quaternion.LookRotation(nextPosition - transform.position);
+        transform.position = nextPosition;
+    }
+}
---
+++ Assets/Scripts/Butterflies/ButterflySpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflySpawner : MonoBehaviour
+{
+    public GameObject butterflyPrefab;
+
+    public int spawnCount = 10;
+
+    public float spawnRadius = 4.0f;
+
+    [Range(0.1f, 20.0f)]
+    public float velocity = 10.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float velocityVariation = 0.5f;
+
+    void Start()
+    {
+        for (var i = 0; i < spawnCount; i++) Spawn();
+    }
+
+    public GameObject Spawn()
+    {
+        Vector3 position = transform.position + Random.insideUnitSphere * spawnRadius;
+        var rotation = Quaternion.Slerp(transform.rotation, Random.rotation, 0.3f);
+        var butterfly = Instantiate(butterflyPrefab, position, rotation) as GameObject;
+        butterfly.GetComponent<ButterflyBehavior>().controller = this;
+		butterfly.GetComponent<ButterflyBehavior>().origin = transform.position;
+		butterfly.GetComponent<ButterflyBehavior>().velBase = velocity;
+		butterfly.GetComponent<ButterflyBehavior>().velVariation = velocity * velocityVariation;
+        return butterfly;
+    }
+}
