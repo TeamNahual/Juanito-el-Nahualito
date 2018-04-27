099bd08b72bc2e256c1151ec64b16a57527aca6f
Mason Reed <machreed@ucsc.edu>
2018-04-26 22:57:45 -0700

    Add function Attack() to call attack animation
    Attach this script to a deer and then use the function to use any of it's  attacks

---
+++ Assets/Scripts/AnimalAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MalbersAnimations.Events;
+using MalbersAnimations.Utilities;
+using MalbersAnimations;
+
+public class AnimalAttack : MonoBehaviour {
+	//Use this function to call an attack animation
+	//Animations are 1-4, with the default being 1
+	//NOTE: Object must also have Animal script attached
+	void Attack(int index = 1)
+	{
+		Debug.Log("attack " + index);
+		gameObject.GetComponent<Animal>().SetAttack(index);
+	}
+}
