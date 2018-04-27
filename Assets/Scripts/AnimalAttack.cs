41baa5db9aa159029e9fac8291b48dbfc614f34f
Mason Reed <machreed@ucsc.edu>
2018-04-26 23:31:03 -0700

    Change to string as parameter

--- Assets/Scripts/AnimalAttack.cs
+++ Assets/Scripts/AnimalAttack.cs
@@ -7,12 +7,25 @@ using MalbersAnimations.Utilities;
 using MalbersAnimations;
 
 public class AnimalAttack : MonoBehaviour {
+	Dictionary<string, int> map;
+	string four = "AttackBackKick";
+	string three = "AttackHornDown";
+	string two  = "AttackHornUp";
+	string one = "AttackRearUp";
+	void Start(){
+		map = new Dictionary<string, int>();
+		map.Add(one, 1);
+		map.Add(two, 2);
+		map.Add(three, 3);
+		map.Add(four, 4);
+	}
+
 	//Use this function to call an attack animation
-	//Animations are 1-4, with the default being 1
+	//Animations are {AttackBackKick, AttackHornDown, AttackHornUp, AttackRearUp},
+	//	with the default being AttackRearUp
 	//NOTE: Object must also have Animal script attached
-	void Attack(int index = 1)
+	void Attack(string index = "AttackRearUp")
 	{
-		Debug.Log("attack " + index);
-		gameObject.GetComponent<Animal>().SetAttack(index);
+		gameObject.GetComponent<Animal>().SetAttack(map[index]);
 	}
 }
