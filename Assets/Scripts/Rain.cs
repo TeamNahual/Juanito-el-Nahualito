08854a135a74d8b007660705e47750c4d4610633
Kyle <kaos17@cableone.net>
2018-05-19 20:03:07 -0700

    Bird-form rain mechanic

---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour {
+
+	public GameObject cloud;
+	public GameObject grass;
+	public Material gray;
+	Vector3 spot;
+
+	void Start()
+	{
+		spot = grass.transform.position;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("collision");
+		this.StartCoroutine (storm());
+	}
+
+	IEnumerator storm()
+	{
+		yield return new WaitForSeconds (1);
+		cloud.GetComponent<MeshRenderer> ().material = gray;
+		yield return new WaitForSeconds (2);
+		while (grass.transform.position.y < -86.7) 
+		{
+			spot.y += 1;
+			grass.transform.position = spot;
+			yield return new WaitForEndOfFrame ();
+		}
+		Destroy (this.gameObject);
+	}
+}
