bc6743b7288f761c1f317c626af5e7d58269de1c
CarpeSwag <james@garbagnati.com>
2018-06-02 17:07:17 -0700

    Color coded subtitles.

--- Assets/Scripts/LocationTriggers/DialogueTrigger.cs
+++ Assets/Scripts/LocationTriggers/DialogueTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine.Audio;
 public class DialogueTrigger : MonoBehaviour {
 	public bool deleteOnEnter = false;
 	public string[] messages;
+	public string[] colorCoded;
 	public int[] timers;
 	public AudioClip[] audioClips;
 	public bool[] locksMovement;
@@ -17,6 +18,38 @@ public class DialogueTrigger : MonoBehaviour {
 		GetComponent<Renderer>().enabled = false;
 	}
 	
+	string colorCodeString(string message, string colorCode) {
+		string START_COLOR = "<color=#facadeff>";
+		string END_COLOR = "</color>";
+		
+		string output = "";
+		
+		string[] messageTokens = message.Split(' ');
+		string[] colorCodeTokens = colorCode.Split(' ');
+		int c = 0;
+		string space = "";
+		bool color = false;
+		
+		for (int i = 0; i < messageTokens.Length; ++i) {
+			string code = "";
+			if (c < colorCodeTokens.Length && messageTokens[i] == colorCodeTokens[c]) {
+				if (!color) {
+					color = true;
+					code = START_COLOR;
+				}
+				++c;
+			} else {
+				if (color) {
+					color = false;
+					code = END_COLOR;
+				}
+			}
+			output += space + code + messageTokens[i];
+			space = " ";
+		}
+		return output;
+	}
+	
     void OnTriggerEnter(Collider other) {
 
     	if(other.gameObject != Juanito.ins.JuanitoHuman)
@@ -25,8 +58,16 @@ public class DialogueTrigger : MonoBehaviour {
 		voiceOver.TransitionTo (.25f);
 		//Debug.Log ("Should have Transitioned");
 
+    	if(other.gameObject != Juanito.ins.JuanitoHuman)
+    		return;
+
+		string[] modifiedMessages = new string[messages.Length];
+		for (int i = 0; i < messages.Length; ++i) {
+			modifiedMessages[i] = colorCodeString(messages[i],
+				(i < colorCoded.Length)? colorCoded[i]: "");
+		}
 		UIManager.instance.dialogueSystem.addDialogue(
-			messages, timers, audioClips, locksMovement);
+			modifiedMessages, timers, audioClips, locksMovement);
 		if (deleteOnEnter) {
 			Destroy(gameObject);
 		}
