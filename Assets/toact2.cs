08ba717c16fa5afb6e106bd25ba00a1773a02eec
Christopher Huynh <chphuynh@ucsc.edu>
2018-06-04 15:29:39 -0700

    grandpashouse transitions to act2cutscene

---
+++ Assets/toact2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class toact2 : MonoBehaviour {
+
+	public PlayableDirector timeline;
+	float timeLineDuration;
+	private float timeLineCurrent;
+
+
+	// Use this for initialization
+	void Start () {
+		timeLineDuration = (float) timeline.duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLineCurrent = (float) timeline.time;
+
+		if (timeLineCurrent >= timeLineDuration - 0.5f) {
+			GameManager.instance.loadLevel("Act2Cutscene");
+		}
+	}
+}
