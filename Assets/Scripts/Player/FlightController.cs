cfdf20fd856131d6387da90210802ff1dbdc5f4a
Jonathan Morales <jonmora647@gmail.com>
2018-08-14 05:39:42 -0700

    Quetzal Controller Changes
    -control direction of flight

--- Assets/Scripts/Player/FlightController.cs
+++ Assets/Scripts/Player/FlightController.cs
@@ -8,17 +8,38 @@ public class FlightController : MonoBehaviour {
 
 	float smooth = 5.0f;
 	float angle = 45.0f;
+	float fullTurn = 180f;
+	float addingDegree = 0f;
 	Vector3 spot;
 	Animator controller;
+	public Vector3 birdX;
+	private Vector3 localPos;
+
+	public Rigidbody birdRigidBody;
+	public float birdFwrd;
+	public float birdHSpeed;
+	private float maxYRotation;
+	private float minYRotation;
+	private float yRotation;
 
 	// Use this for initialization
 	void Start () {
 		spot = transform.localPosition;
 		controller = GetComponentInChildren<Animator> ();
+
+		birdRigidBody = GetComponent<Rigidbody> ();
+		birdFwrd = 50.0f;
+		birdHSpeed = 20.0f;
+
+		//Rotation Values
+		maxYRotation = 0.4f;
+		minYRotation = 0.4f;
+
+		localPos = transform.localPosition;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		float h = CrossPlatformInputManager.GetAxis("Horizontal");
 		float v = CrossPlatformInputManager.GetAxis("Vertical");
 		float s;
@@ -44,13 +65,43 @@ public class FlightController : MonoBehaviour {
 		}
 
 		//Applying inputs to rotations
-		Quaternion target = Quaternion.Euler(-v*angle,0,-h*angle);
+		Quaternion target = Quaternion.Euler(-v*angle, addingDegree,-h*angle);
+		//Quaternion ySave = Quaternion.Euler (v*angle, 0, h*angle);
+		//yRotation = transform.localRotation.y;
+		//yRotation = h * fullTurn;
+		Debug.Log ("Quaternion Rotation: " + target);
 		//Return to resting rotation when not pressing buttons
 		transform.localRotation = Quaternion.Slerp (transform.localRotation, target, Time.deltaTime * smooth);
+
 		//Move around the screen with inputs, bounded at the edges
-		spot.x += (h/10);
-		spot.y += (v/10);
-		if (spot.x > 8f) 
+		//spot.x += (h/3);
+		//spot.y += (v/10);
+
+		//automatically move forward
+		birdRigidBody.velocity = transform.forward * birdFwrd;
+
+		//Debug.Log ("Rigid Body: " + birdRigidBody.velocity);
+		//Move to the sides
+		if (h > 0) {
+			//birdRigidBody.velocity = transform.right * birdHSpeed;
+			//birdRigidBody.velocity = new Vector3 (birdHSpeed, 0f, birdFwrd);
+			birdRigidBody.velocity = (transform.right * birdHSpeed) + (transform.forward * birdFwrd);
+			yRotation = h * fullTurn;
+
+			// Turns bird in Quaternion target
+			addingDegree += 0.75f;
+		}
+		if (h < 0) {
+			//birdRigidBody.AddForce(transform.right * -birdHSpeed);
+			//birdRigidBody.velocity = new Vector3 (-birdHSpeed, 0f, birdFwrd);
+			birdRigidBody.velocity = (transform.right * -birdHSpeed) + (transform.forward * birdFwrd);
+
+			// Turns bird in Quaternion target
+			addingDegree -= 0.75f;
+		}
+
+		//gameObject.transform.forward
+		/*if (spot.x > 8f) 
 		{
 			spot.x = 8f;
 			Debug.Log ("right");
@@ -69,8 +120,11 @@ public class FlightController : MonoBehaviour {
 		{
 			spot.y = -4f;
 			Debug.Log ("down");
-		}
-		transform.localPosition = spot;
+		}*/
+		localPos.x = spot.x;
+		localPos.y = 0;
+		//localPos.z = speed; 
+		//transform.localPosition = localPos;
 		if (v == 0) 
 		{
 			v = -1;
